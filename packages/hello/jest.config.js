11511ad24637d32ce1da11034cc368bcd6ed4cf8
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-04-08 15:38:59 +0900

    🍴 chore: add typescript, jest

---
+++ packages/hello/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "coverageDirectory": "./coverage/",
+  "collectCoverage": false,
+  "collectCoverageFrom": [
+    "src/**/*.{ts,tsx}"
+  ]
+}
