0a5e6750ca2804dee049057e7f262b4a622278bd
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-04-09 19:12:12 +0900

    🍏 feat: add doGet method and html file

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -1,5 +1,12 @@
 import { showHello, getHello } from './hello';
 
-function myFunction() {
+export function myFunction() {
   showHello(getHello());
+}
+
+// HTTP GETによる実行エントリポイント
+// Google Apps Script でHTMLファイルを作って表示。 - Qiita - https://qiita.com/taromorimotohf/items/5e52cb9062600e8ccac3
+function doGet() {
+  const template = 'myfunction';
+  return HtmlService.createTemplateFromFile(template).evaluate();
 }
