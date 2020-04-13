bf091eb317e4f1b0e568622ce0402f1ff0934b14
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-04-13 14:45:14 +0900

    🥩 feat: add createSS function

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -14,6 +14,26 @@ function myFunction() {
 
 // myfunction.html から呼び出される関数
 function getSpreadsheetName(hoge: string) {
-  // return SpreadsheetApp?.getActiveSpreadsheet()?.getName();
-  return `Called getSpreadsheetName! ${hoge} foo`;
+  return `${SpreadsheetApp?.getActiveSpreadsheet()?.getName()} ${hoge}`;
+}
+
+// IDをどのように知るかと言いますと、フォルダであればGoogleドライブでそのフォルダを開いている際のURLの
+// https://drive.google.com/drive/folders/{フォルダID}
+// の部分です。
+// Google Apps ScriptでGoogleドライブを操作する最も簡単なスクリプト - https://tonari-it.com/gas-google-drive-app/
+const GAS_FOLDER_ID = '1S4-Wz1p5GLyDm9i42lFZ3iSj2ofQOitU';
+function creatSS() {
+  // スプレッドシートオブジェクトを作成する
+  const id = SpreadsheetApp.create('new Spread Sheet').getId();
+  // スプレッドシートのファイルオブジェクトを取得する
+  const file = DriveApp.getFileById(id);
+
+  // スプレッドシートの移動先となるフォルダー名
+  const folder = DriveApp.getFolderById(GAS_FOLDER_ID);
+
+  // ルートに出来たスプレッドシートをフォルダーに移動して、ルートにある同ファイルを削除する
+  folder.addFile(file);
+  DriveApp.getRootFolder().removeFile(file);
+
+  console.log(folder.getName());
 }
