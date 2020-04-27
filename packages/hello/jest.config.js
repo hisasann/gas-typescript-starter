0d410c1a8d6dd8c475ec16ed75676ca3d0143772
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-04-27 13:57:50 +0900

    🥗 chore: change jest trasform module from ts-jest to babel-jest `??` がビルドできないため babel-jest に変更

--- packages/hello/jest.config.js
+++ packages/hello/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'babel-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
